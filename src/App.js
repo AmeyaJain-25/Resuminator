b09d627b44e967261b46e3268bafb24c5cdcd7e7
AmeyaJain-25 <ameiya.jain@hotmail.com>
2021-03-02 23:33:00 +0530

    Bootstrap removed from app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { Button, Jumbotron, Container } from "reactstrap"
 import { Link } from 'react-router-dom';
-import "bootstrap/dist/css/bootstrap.min.css"
 
 const App = () => {
   return (
